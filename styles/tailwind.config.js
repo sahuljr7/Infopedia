eb3a06c6a50c4da5dd9232393614e0a3f69064f5
Sahul Kumar Parida <71061738+sahuljr7@users.noreply.github.com>
2024-04-20 16:53:42 +0530

    Update tailwind.config.js

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,5 +1,6 @@
 const autoprefixer = require('autoprefixer');
 const { fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   important: true,
